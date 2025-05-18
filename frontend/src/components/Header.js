813085a37ec7b06872afe6d7940350607aa51e74
Andreia Semedo <112090817+Andreia797@users.noreply.github.com>
2025-05-18 14:16:06 -0100

    Atualizado

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,12 +11,21 @@ function Header() {
     navigate('/login');
   };
 
+  const goToDashboard = () => {
+    navigate('/dashboard'); // Certifique-se de que '/dashboard' é o caminho correto para o seu dashboard
+  };
+
   return (
-    <header className="bg-blue-600 p-3 shadow-md"> 
+    <header className="bg-blue-600 p-5 shadow-md">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center">
-          <img src={unicvLogo} alt="UniCV Logo" className="h-8 mr-3" /> 
-          <h1 className="text-lg font-semibold text-black"> 
+          <button
+            onClick={goToDashboard}
+            className="flex items-center bg-transparent border-none p-0 cursor-pointer" 
+          >
+            <img src={unicvLogo} alt="UniCV Logo" className="h-10 mr-3" /> 
+          </button>
+          <h1 className="text-2xl font-semibold text-black">
             Plataforma de Gestão das Residências da Uni-CV
           </h1>
         </div>
